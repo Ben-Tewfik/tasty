0d8e07087de8a64b0fc0994240e22a27e317da49
benarba mohammed <tewfikhotspur@gmail.com>
2024-07-24 20:23:28 +0100

    wrap recipeComponent and layout around app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import Layout from "./Layout";
+import RecipesContext from "@/context/recipesContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <RecipesContext>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </RecipesContext>
+  );
 }
