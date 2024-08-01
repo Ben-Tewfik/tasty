6d882ca4dcff618b150e6c508f31d9279962088e
benarba mohammed <tewfikhotspur@gmail.com>
2024-08-01 13:53:37 +0100

    display data in recipeCategories

--- components/RecipeCategories.jsx
+++ components/RecipeCategories.jsx
@@ -1,15 +1,13 @@
-import { useGlobalContext } from "@/contexts/RecipesContext";
 import SectionTitle from "./SectionTitle";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function RecipeCategories() {
-  const { recipeCategories, setCategory } = useGlobalContext();
+export default function RecipeCategories({ categories }) {
   return (
     <section className="w-[90%] max-w-[1170px] mx-auto py-10">
       <SectionTitle>highlights</SectionTitle>
       <div className="grid grid-cols-3 gap-y-8 md:grid-cols-5 lg:grid-cols-7">
-        {recipeCategories.map(mealsCategory => {
+        {categories.map(mealsCategory => {
           const {
             idCategory: id,
             strCategory: category,
@@ -17,10 +15,9 @@ export default function RecipeCategories() {
           } = mealsCategory;
           return (
             <Link
-              href={`/category/${category}`}
+              href={`/categories/${category}`}
               key={id}
               className="text-center mx-auto block w-[100px]"
-              onClick={() => setCategory(category)}
             >
               <Image
                 src={strCategoryThumb}
