6281a6fec00bd031cb09b8b6a2b9899b5fee8395
benarba mohammed <tewfikhotspur@gmail.com>
2024-08-01 13:51:03 +0100

    delete useEffect functions in recipeContext to try to fetch data in another component using ssg

--- contexts/RecipesContext.jsx
+++ contexts/RecipesContext.jsx
@@ -4,21 +4,8 @@ import { createContext, useContext, useEffect, useState } from "react";
 const GlobalContext = createContext();
 
 export default function RecipesContext({ children }) {
-  const [recipeCategories, setRecipeCategories] = useState([]);
-  // fetch recipe categories
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const response = await axios(
-        "https://www.themealdb.com/api/json/v1/1/categories.php"
-      );
-      setRecipeCategories(response.data.categories);
-    };
-    fetchCategories();
-  }, []);
   return (
-    <GlobalContext.Provider value={{ recipeCategories }}>
-      {children}
-    </GlobalContext.Provider>
+    <GlobalContext.Provider value={"hello"}>{children}</GlobalContext.Provider>
   );
 }
 export const useGlobalContext = () => useContext(GlobalContext);
