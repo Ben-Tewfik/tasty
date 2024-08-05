bc82327da3314863c2dedd049569476598133ad5
benarba mohammed <tewfikhotspur@gmail.com>
2024-08-05 13:51:57 +0100

    add margin top to sectionTitle Component

--- components/SectionTitle.jsx
+++ components/SectionTitle.jsx
@@ -1,6 +1,6 @@
 export default function SectionTitle(props) {
   return (
-    <h1 className="text-red text-center mb-8 capitalize font-bold text-xl">
+    <h1 className="text-red text-center my-8 capitalize font-bold text-xl">
       {props.children}
     </h1>
   );
