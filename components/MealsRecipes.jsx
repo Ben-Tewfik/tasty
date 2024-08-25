6b5d33df400596162f7f4c7f0fafb7c948aabd3a
benarba mohammed <tewfikhotspur@gmail.com>
2024-08-25 10:44:59 +0100

    add a margin bottom to seperate the footer from the component

--- components/MealsRecipes.jsx
+++ components/MealsRecipes.jsx
@@ -2,7 +2,7 @@ import MealCard from "./MealCard";
 
 export default function MealsRecipes({ meals }) {
   return (
-    <section className="w-[90vw] mx-auto max-w-[1170px] grid grid-cols-auto-fill gap-4">
+    <section className="w-[90vw] mx-auto mb-10 max-w-[1170px] grid grid-cols-auto-fill gap-4">
       {meals?.map(filteredMeal => {
         return <MealCard key={filteredMeal.id} {...filteredMeal} />;
       })}
