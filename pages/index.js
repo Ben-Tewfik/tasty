f193ce8dc98dafd946907b17e63881d6f43c449c
benarba mohammed <tewfikhotspur@gmail.com>
2024-08-26 13:15:49 +0100

    fetch random meal and pass as a prop to random meal component

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,31 @@ import Image from "next/image";
 import RecipeCategories from "@/components/RecipeCategories";
 import Head from "next/head";
 import axios from "axios";
+import RandomMeal from "@/components/RandomMeal";
 
 export async function getStaticProps() {
   try {
+    // fetch categories
     const response = await axios(
       "https://www.themealdb.com/api/json/v1/1/categories.php"
     );
-
-    return { props: { categories: response.data.categories } };
+    // fetch random meal
+    const response2 = await axios(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    return {
+      props: {
+        categories: response.data.categories,
+        randomMeal: response2.data.meals[0],
+      },
+    };
   } catch (error) {
     console.error("can not fetch data", error);
     return { props: { categories: [] } };
   }
 }
 
-export default function Home({ categories }) {
+export default function Home({ categories, randomMeal }) {
   return (
     <>
       <Head>
@@ -24,6 +34,7 @@ export default function Home({ categories }) {
       </Head>
 
       <RecipeCategories categories={categories} />
+      <RandomMeal randomMeal={randomMeal} />
     </>
   );
 }
