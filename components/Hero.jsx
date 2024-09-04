9b403e517679a661683903bf068df5f4016dccfc
benarba mohammed <tewfikhotspur@gmail.com>
2024-09-04 22:24:05 +0100

    adjust the text size of hero component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-import tasty from "@/public/tasty-hero.jpg";
 export default function Hero() {
   return (
     <section
       style={{
         backgroundImage: `url('/tasty-hero.jpg')`,
       }}
-      className="flex flex-col justify-center items-center text-dark font-bold text-3xl h-[50vh] bg-cover sm:h-[60vh] md:h-[70vh] lg:h-[91vh] bg-no-repeat bg-center"
+      className="flex flex-col justify-center items-center text-dark font-bold text-3xl sm:text-4xl md:text-5xl lg:text-7xl text-x h-[50vh] bg-cover sm:h-[60vh] md:h-[70vh] lg:h-[91vh] bg-no-repeat bg-center"
     >
-      <h1 className="">Your favorite food</h1>
-      <h3 className="ml-16">Make it good</h3>
+      <div className="flex flex-col items-end lg:gap-2">
+        <h1>Your favorite food</h1>
+        <h3>Make it good</h3>
+      </div>
     </section>
   );
 }
