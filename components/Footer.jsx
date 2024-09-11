593fbcc72ae0ac5d20bf966df778c39f4fe244b5
benarba mohammed <tewfikhotspur@gmail.com>
2024-09-11 21:02:57 +0100

    create initial simple footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,24 @@
+import { Pacifico } from "next/font/google";
+import Link from "next/link";
+const pacifico = Pacifico({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-pink rounded-tl-3xl rounded-tr-3xl">
+      <div className="w-[90vw] mx-auto max-w-[1170px] p-5">
+        {/* logo */}
+        <Link
+          href={"/"}
+          className={`${pacifico.className} text-3xl md:text-5xl block text-dark text-center`}
+        >
+          Tasty
+        </Link>
+        <p className="text-center mt-5 font-semibold text-sm">
+          Copyright &copy; {new Date().getFullYear()} Tasty All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
 }
