d042ccec905ca4f7f5893b5d8a5c6ed1776a2a20
benarba mohammed <tewfikhotspur@gmail.com>
2024-09-25 20:48:57 +0100

    add testimonials info into testimonial file and start the logic to map over them to diplay them based on the user clicks

--- components/Testimony.jsx
+++ components/Testimony.jsx
@@ -1,16 +1,43 @@
+import Image from "next/image";
+import testimonial1 from "@/public/testimonial 1.jpg";
+import { testimonials } from "@/utils/testimonials";
+import { useState } from "react";
 export default function Testimony() {
+  const [position, setPosition] = useState(0);
+  const { name, image, testimony } = testimonials[position];
   return (
-    <section>
-      <div>
-        <h1>testimony</h1>
-        <p>hi</p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque minima
-          totam consequatur ipsum, placeat sit cumque. Nesciunt blanditiis
-          consequatur minima.
-        </p>
+    <section className="bg-grey py-7 relative">
+      <div className="w-[90vw] max-w-[1170px] mx-auto">
+        <h1 className="capitalize text-xl text-red font-semibold mb-7">
+          testimony
+        </h1>
+        {/* image */}
+        <div>
+          <Image
+            src={image}
+            alt="testimonial image"
+            width={100}
+            height={100}
+            className="block absolute right-7 -top-7 rounded-full w-52 h-52 object-cover"
+          />
+          <div className="flex gap-2 absolute right-[107px] bottom-16">
+            {testimonials.map((testimonial, index) => {
+              return (
+                <div
+                  key={testimonial.name}
+                  className={`w-2 h-2 rounded-full cursor-pointer bg-pink ${
+                    index === position ? "bg-red" : null
+                  }`}
+                ></div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-red capitalize">Hi, I&apos;m {name}</p>
+          <p className="max-w-64">{testimony}</p>
+        </div>
       </div>
-      {/* image */}
     </section>
   );
 }
---
+++ utils/testimonials.js
@@ -0,0 +1,20 @@
+export const testimonials = [
+  {
+    name: "james forehand",
+    image: "/testimonial 1.jpg",
+    testimony:
+      "Perfect and delicious!!! Why dine out? Save the money and make this at home",
+  },
+  {
+    name: "shahid khan",
+    image: "/testimonial 2.jpg",
+    testimony:
+      "I love how easy it is to find new recipes on this site! The search feature is super intuitive, and the variety of meals is amazing. Whether I’m looking for something healthy or indulgent, I always find something that fits my mood. Definitely my go-to place for meal inspiration!",
+  },
+  {
+    name: "gostavo sanchez",
+    image: "/testimonial 3.jpg",
+    testimony:
+      "This site makes cooking at home so much more fun! The meal details are spot on, and I’ve learned a lot about international cuisine just by browsing through the categories. I even tried some new dishes I’d never heard of before. Thanks for making meal planning a breeze!",
+  },
+];
