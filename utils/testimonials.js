663f7601eda0beed3018d1c67cf1def40b851a6a
benarba mohammed <tewfikhotspur@gmail.com>
2024-09-28 18:16:23 +0100

    make the testimonials paragraph shorter

--- utils/testimonials.js
+++ utils/testimonials.js
@@ -9,12 +9,12 @@ export const testimonials = [
     name: "shahid khan",
     image: "/testimonial 2.jpg",
     testimony:
-      "I love how easy it is to find new recipes on this site! The search feature is super intuitive, and the variety of meals is amazing. Whether I’m looking for something healthy or indulgent, I always find something that fits my mood. Definitely my go-to place for meal inspiration!",
+      "I love how easy it is to find new recipes on this site! The search feature is super intuitive.",
   },
   {
     name: "gostavo sanchez",
     image: "/testimonial 3.jpg",
     testimony:
-      "This site makes cooking at home so much more fun! The meal details are spot on, and I’ve learned a lot about international cuisine just by browsing through the categories. I even tried some new dishes I’d never heard of before. Thanks for making meal planning a breeze!",
+      "This site makes cooking at home so much more fun! The meal details are spot on.",
   },
 ];
