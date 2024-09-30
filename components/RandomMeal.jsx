b2359d5233b5360df5eff954f8897a65e67cbca9
benarba mohammed <tewfikhotspur@gmail.com>
2024-09-30 19:51:40 +0100

    fix redirecting user into home page when the profile page is refreshed and delete console log in reandom meals component

--- components/RandomMeal.jsx
+++ components/RandomMeal.jsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function RandomMeal({ randomMeal }) {
-  console.log(randomMeal);
   const {
     idMeal: id,
     strMealThumb: img,
--- pages/profile.js
+++ pages/profile.js
@@ -2,16 +2,19 @@ import { Loader } from "@/components/Loader";
 import UserProfile from "@/components/UserProfile";
 import { useGlobalContext } from "@/contexts/RecipesContext";
 import { useRouter } from "next/router";
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 export default function Profile() {
   const { currentUser, loading } = useGlobalContext();
   const router = useRouter();
   // making the route protective
-  useLayoutEffect(() => {
-    if (!currentUser) {
+  useEffect(() => {
+    if (!loading && !currentUser) {
       router.push("/");
     }
-  }, []);
+  }, [currentUser, loading, router]);
+  console.log(currentUser);
+
+  console.log(loading);
 
   if (loading || (!loading && !currentUser)) {
     return (
